db0645b32ad6f2a56c4d34aee1e4f125f9169fdc
dimitrigalli <dimitrigalli@air-di-dimitri.homenet.telecomitalia.it>
2019-07-17 11:23:50 +0200

    update

--- VotingSystem/src/votingsystem/MainPanel.java
+++ VotingSystem/src/votingsystem/MainPanel.java
@@ -1,5 +1,12 @@
 package votingsystem;
 
+import java.awt.BorderLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /**
@@ -9,9 +16,32 @@ import javax.swing.JPanel;
 public class MainPanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private BufferedImage image1;
+	private BufferedImage image2;
+	private JLabel imagelabel1;
+	private JLabel imagelabel2;
+	private JPanel northpanel, southpanel;
 	
 	public MainPanel() {
 		super();
+		try {
+			image1 = ImageIO.read(new File("images"+File.separator+"Logo-DIEF.png"));
+			image2 = ImageIO.read(new File("images"+File.separator+"Logo-MIUR.png"));
+		} catch (IOException e) {
+			System.out.println("Error: images \"Logo-DIEF.png\" and \"Logo-MIUR.png\" not found\n");
+			e.printStackTrace();
+		}
+		imagelabel1 = new JLabel(new ImageIcon(image1));
+		imagelabel2 = new JLabel(new ImageIcon(image2));
+		imagelabel1.setSize(100, 100);
+		imagelabel2.setSize(100, 100);
+		northpanel = new JPanel(new BorderLayout());
+		northpanel.add(imagelabel1, BorderLayout.WEST);
+		southpanel = new JPanel(new BorderLayout());
+		southpanel.add(imagelabel2, BorderLayout.EAST);
+		this.setLayout(new BorderLayout());
+		this.add(northpanel, BorderLayout.NORTH);
+		this.add(southpanel, BorderLayout.SOUTH);
 	}
 
 }
